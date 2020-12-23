3c531e792a46af364df5e4a33e899c6f08e45586
Sonu Saini <sonu2311saini@gmail.com>
2020-12-23 11:51:34 +0530

    First commit

---
+++ flipkart/step5/products.js
@@ -0,0 +1,221 @@
+var all_products_list=[
+		{
+			"link" : "https://rukminim1.flixcart.com/image/492/590/jvfk58w0/dress/e/d/u/m-2520961-dressberry-original-imafgbmjgdr5ybhz.jpeg?q=50",
+			"price" : 240,
+			"dress_type":"shirt",
+			"dress_color":"grey",
+			"title": "Laptop",
+			"brand":"max",
+			"discount":5%,
+			"content":"ye wehj kj hkhk khh kh dksh .hfd khsk hdk"
+		},
+		{
+			"link" : "https://rukminim1.flixcart.com/image/492/590/kesv0y80/dress/d/y/m/l-rd-2001-radius-export-import-original-imafven4vvmrv3px.jpeg?q=50",
+			"price" : 290,
+			"dress_type":"western frok",
+			"dress_color":"white",
+			"title": "Laptop",
+			"brand":"zara",
+			"discount":5%,
+			"content":"ye wehj d sh k ahfkd hs .hfd khsk hdk"
+
+		},
+		{
+			"link" : "https://rukminim1.flixcart.com/image/492/590/jpu324w0/dress/z/b/j/m-aa-0119-boho-long-dress-aayu-original-imafbz74rzgwhztq.jpeg?q=50",
+			"dress_type":"maxi ",
+			"dress_color": "brown",
+			"price" : 40000,
+			"title": "Laptop",
+			"brand":"biba",
+			"discount":5%,
+			"content":"ye wehj kahfkd hs .hfd khsk hdk"
+
+		},
+		{
+			"link" : "https://rukminim1.flixcart.com/image/492/590/jxuq07k0/dress/p/y/v/free-dress-1010-rangun-original-imafg7n76fkyuuws.jpeg?q=50",
+			"dress_type":"maxi",
+			"dress_color": "black",
+			"price" : 240,
+			"title": "Laptop",
+			"brand":"biba",
+			"discount":5%,
+			"content":"ye weh ahfkd hs .hfd khsk hdk"
+
+		},
+		{
+			"link" : "https://rukminim1.flixcart.com/image/492/590/k572gsw0/dress/e/u/y/s-maxy-dress-196-black-daevish-original-imafnxw5wwtgjp3q.jpeg?q=50",
+			"dress_type":"maxi",
+			"dress_color": "black",
+			"price" : 900,
+			"title": "Laptop",
+			"brand":"biba",
+			"discount":5%,
+			"content":"ye ahfkd hs .hfd khsk hdk"
+
+		},
+		{
+			"link" : "https://rukminim1.flixcart.com/image/492/590/k45x0nk0/dress/g/b/k/m-western-dress-dr39a-metro-fashion-original-imafn4mc5gcyanvm.jpeg?q=50",
+			"dress_type":"western short dress",
+			"dress_color": "black",
+			"price" : 5540,
+			"title": "Laptop",
+			"brand":"biba",
+			"discount":5%,
+			"content":"ye fkd hs .hfd khsk hdk"
+
+		},
+		{
+			"link" : "https://rukminim1.flixcart.com/image/492/590/jwfa5jk0/dress/4/d/g/m-gr5759-black-harpa-original-imafh43kuk5z295k.jpeg?q=50",
+			"dress_type":"maxi",
+			"dress_color": "red",
+			"price" : 240,
+			"title": "Laptop",
+			"brand":"trands",
+			"discount":5%,
+			"content":"ye  ahfkd hs .hfd khsk hdk"
+
+		},
+		{
+			"link" : "https://rukminim1.flixcart.com/image/492/590/k76ihe80/dress/n/c/d/s-one-piece-orrange-line-maa-fab-original-imafph3nakcmenhr.jpeg?q=50",
+			"dress_type":"western short dress",
+			"dress_color":"yellow" ,
+			"price" : 1240,
+			"title": "Laptop",
+			"brand":"trands",
+			"discount":5%,
+			"content":"ye wehj "
+
+		},
+		{
+			"link" : "https://rukminim1.flixcart.com/image/492/590/kar44280/dress/s/u/g/xs-gaurgeous780-gaurgeous-original-imafs9g4w3zqjzup.jpeg?q=50",
+			"dress_type":"maxi",
+			"dress_color": "pink",
+			"price" : 2240,
+			"title": "Laptop",
+			"brand":"trands",
+			"discount":75%,
+			"content":"ye wehj kj hkhk khh kh dksh. khfkdh kl hfd sh k ahfkd hs .hfd khsk hdk"
+
+		},
+		{
+			"link" : "https://rukminim1.flixcart.com/image/492/590/juzuavk0/dress/c/c/j/l-il-d-31-illi-london-original-imaffy6g4fybwznb.jpeg?q=50",
+			"dress_type":"maxi",
+			"dress_color": "blue",
+			"price" : 240,
+			"title": "Laptop",
+			"brand":"zara",
+			"discount":45%,
+			"content":"ye wehh k ahfkd hs .hfd khsk hdk"
+
+		},
+		{
+			"link" : "https://rukminim1.flixcart.com/image/492/590/k45x0nk0/dress/c/v/m/free-amira-dress-gofan-original-imafjbufzzggdyjq.jpeg?q=50",
+			"dress_type":"maxi",
+			"dress_color": "blue",
+			"price" : 600,
+			"title": "Laptop",
+			"brand":"zara",
+			"discount":53%,
+			"content":"ye wehj kj hkhk khh kh dksh. khfkdh kl hfd sh k ahfkd hs .hfd khsk hdk"
+
+		},
+		{
+			"link" : "https://rukminim1.flixcart.com/image/492/590/ke1pnrk0/dress/s/h/t/xs-gbk1214-god-bless-original-imafusujapbshjra.jpeg?q=50",
+			"dress_type":"western frok",
+			"dress_color": "pink",
+			"price" : 740,
+			"title": "Laptop",
+			"brand":"zara",
+			"discount":15%,
+			"content":"ye wehj hs .hfd khsk hdk"
+
+		},
+		{
+			"link" : "https://rukminim1.flixcart.com/image/492/590/k6wiefk0/dress/r/g/q/l-zf090-ziya-fashion-original-imafp8yahwfgmxhm.jpeg?q=50",
+			"dress_type":"maxi",
+			"dress_color": "black",
+			"price" : 800,
+			"title": "Laptop",
+			"brand":"lifestyle",
+			"discount":20%,
+			"content":"ye wehs .hfd khsk hdk"
+
+		},
+		{
+			"link" : "https://rukminim1.flixcart.com/image/492/590/k3orqfk0/dress/6/g/d/xl-dr-18-zakar-style-original-imafem2cupdez6qw.jpeg?q=50",
+			"dress_type":"western frok",
+			"dress_color": "white",
+			"price" : 240,
+			"title": "Laptop",
+			"brand":"lifestyle",
+			"discount":90%
+			"content":"ye ahfkd hs .hfd khsk hdk"
+
+		},
+		{
+			"link" : "https://rukminim1.flixcart.com/image/492/590/k6wiefk0/dress/r/g/q/l-zf090-ziya-fashion-original-imafp8yahwfgmxhm.jpeg?q=50",
+			"price" : 540,
+			"dress_type":"maxi",
+			"dress_color": "black",
+			"title": "Laptop",
+			"brand":"lifestyle",
+			"discount":1%,
+			"content":"ye wehj kj hkhk khh kh dksh. khfkdh kl hfd sh k ahfkd hs .hfd khsk hdk"
+
+		},
+		{
+			"link" : "https://rukminim1.flixcart.com/image/492/590/k3ahbww0/dress/c/g/4/l-gn2013-l-dev-fashion-original-imafgnnhm4zkqwcz.jpeg?q=50",
+			"dress_type":"maxi",
+			"dress_color": "blue",
+			"price" : 140,
+			"title": "Laptop",
+			"brand":"westren",
+			"content":"ye wehj kd hs .hfd khsk hdk"
+
+		},
+		{
+			"link" : "https://rukminim1.flixcart.com/image/492/590/k6wiefk0/dress/8/h/q/s-zf092-ziya-fashion-original-imafp8re7eabuudm.jpeg?q=50",
+			"dress_type":"western frok",
+			"dress_color": "black",
+			"price" : 2402,
+			"title": "Laptop",
+			"brand":"westren",
+			"discount":50%,
+			"content":"ye wehjd hs .hfd khsk hdk..... ye weh "
+
+		},
+		{
+			"link" : "https://rukminim1.flixcart.com/image/492/590/kc0u7bk0/dress/8/y/j/l-3655drkstrapdrs-stylestone-original-imaft8zszmfwgfp2.jpeg?q=50",
+			"dress_type":"jumpsuit",
+			"dress_color": "blue",
+			"price" : 2401,
+			"title": "Laptop",
+			"brand":"westren",
+			"discount":65%,
+			"content":"ye wehj hs .hfd khsk hdk"
+
+		},
+		{
+			"link" : "https://rukminim1.flixcart.com/image/492/590/k687wy80/dress/w/d/r/m-848-bhojalram-creation-original-imafj33wtg3svb47.jpeg?q=50",
+			"dress_type":"maxi",
+			"dress_color": "white",
+			"price" : 2403,
+			"title": "Laptop",
+			"brand":"max",
+			"discount":95%,
+			"content":"ye wehj s .hfd khsk hdk"
+
+		},
+		{
+			"link" : "https://rukminim1.flixcart.com/image/492/590/k6gsk280/dress/z/u/n/s-maxi-3-r2d-original-imafzh3rqyr9dzu8.jpeg?q=50",
+			"dress_type":"western frok",
+			"dress_color": "black",
+			"price" : 240,
+			"title": "Laptop",
+			"brand":"max",
+			"discount":10%,
+			"content":"ye wehj hs .hfd khsk hdk"
+
+		},
+
+		]
